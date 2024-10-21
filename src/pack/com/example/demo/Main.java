f5aa4743822f619ddff65616fab944ab34b5e981
Fluxy <mainadres2006@gmail.com>
2024-10-21 14:58:58 +0300

    Все дописал первую страничку, полностью функционирует, корректно отрабатывает фильтрацию и вывод в таблицу данных, доавление данных также корректно работает.

--- src/pack/com/example/demo/Main.java
+++ src/pack/com/example/demo/Main.java
@@ -2,19 +2,24 @@
     package pack.com.example.demo;
     //Библиотека для работы с javaFX
     import javafx.application.Application;
+
+    import java.sql.*;
     import java.time.LocalDate;
     import javafx.fxml.FXMLLoader;
     import javafx.scene.Parent;
     import javafx.scene.Scene;
     import javafx.scene.control.*;
+    import javafx.scene.control.cell.PropertyValueFactory;
     import javafx.stage.Stage;
     import javafx.fxml.FXML; // Импорт аннотации FXML
-    //#region Библиотеки для работы с БД и Исключениями
+    //import sun.util.resources.LocaleData;
+    // Библиотеки для работы с БД и Исключениями
     import java.io.IOException;
-    import java.sql.Connection;
-    import java.sql.DriverManager;
-    import java.sql.SQLException;
-    import java.time.LocalDate;
+    import java.util.*;
+    // Для записи данных в табличку БД
+    import javafx.collections.FXCollections;
+    import javafx.collections.ObservableList;
+    import org.checkerframework.checker.units.qual.A;
 
     public class Main extends Application {
 
@@ -23,6 +28,35 @@
         static String name = "postgres";
         static String pass = "123";
         //#endregion
+
+        //#region Создание переменных для работы со столбцами
+        @FXML
+        private TableView<InfoCarsUsers> MyTableFirstPage; // переменная всей таблицы в целом
+        // каким образом работает: Получается, выбираем столбец, задаем значение, задаем навзание переменной
+        @FXML
+        private TableColumn<InfoCarsUsers, String> carBrandColumn;
+        @FXML
+        private TableColumn<InfoCarsUsers, String> carTypeColumn;
+        @FXML
+        private TableColumn<InfoCarsUsers, String> personTypeColumn;
+        @FXML
+        private TableColumn<InfoCarsUsers, String> licensePlateColumn;
+        @FXML
+        private TableColumn<InfoCarsUsers, LocalDate> yearColumn;
+        @FXML
+        private TableColumn<InfoCarsUsers, String> ownerNameColumn;
+        @FXML
+        private TableColumn<InfoCarsUsers, String> addressColumn;
+        @FXML
+        private TableColumn<InfoCarsUsers, String> engineVolumeColumn;
+        @FXML
+        private TableColumn<InfoCarsUsers, String> colorColumn;
+        @FXML
+        private TableColumn<InfoCarsUsers, String> bodyTypeColumn;
+        @FXML
+        private TableColumn<InfoCarsUsers, String> vinColumn;
+        //#endregion
+
         //#region Объявление выпадающих списков для добавления в них информации
         @FXML
         private ComboBox<String> comBoxFirstPage;
@@ -30,8 +64,6 @@
         private ComboBox<String> comBoxSecondPage; // Для второго таба
         @FXML
         private ComboBox<String> comBoxThirdPage; // Для третьей вкладки
-        @FXML
-        private ComboBox<String> comBoxThourPage; // Для вкладки с добавлением автовладельца
         //#endregion
 
         //#region Объявляем переменные для создания запросов в БД для странички с просмотром информации о владельцах автомобилей
@@ -106,7 +138,7 @@
         public void start(Stage primaryStage) {
             try {
                 System.out.println("Загрузка");
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("../../../../resources/MyFinallInterface.fxml"));
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/MyFinallInterface.fxml"));
                 loader.setController(this);
                 Parent root = loader.load();
                 System.out.println("Интерфейс загружен");
@@ -127,13 +159,14 @@
             }
         }
         //#endregion
-
-        //#region Функция создания отчета
+        //Тут начинаются функции программы тоесть по типу нажали на кнопочку и что то произошло
+        //#region Функция Добавление информации в БД с проверкой
         @FXML
         private void CreateReport() {
-            //Создаем объект нашего класса
+            // Создаем объект нашего класса проверки строк
             StrinMethods meth = new StrinMethods();
-            // записываем в переменные данные введеные пользователем
+
+            // Записываем в переменные данные, введенные пользователем
             String name = nameOfUser.getText();
             String model = modelOfCar.getText();
             String numbersCars = numberOfCar.getText();
@@ -145,24 +178,133 @@
             String carColor = colorOfCar.getText();
             String userAdres = adressOfUser.getText();
             String typeAuto = getTypeAuto();
+
             // Получаем текущую дату для сравнения, чтобы не произошла ошибка случайно.
             LocalDate currentDate = LocalDate.now();
-            //делаем проверку на пустые поля
-            if (meth.isNullOrEmpty(name) == false || meth.isNullOrEmpty(model) == false || meth.isNullOrEmpty(numbersCars) == false
-                    || meth.isNullOrEmpty(typeUser) == false || meth.isNullOrEmpty(valueEngine) == false || meth.isNullOrEmpty(typeBody) == false
-                    || meth.isNullOrEmpty(vinNumber) == false ||  dateCreated == null || meth.isNullOrEmpty(carColor) == false
-                    || meth.isNullOrEmpty(userAdres) == false || meth.isNullOrEmpty(typeAuto) == false) {
+
+            // Проверяем на пустые поля
+            if (meth.isNullOrEmpty(name) || meth.isNullOrEmpty(model) || meth.isNullOrEmpty(numbersCars) ||
+                    meth.isNullOrEmpty(typeUser) || meth.isNullOrEmpty(valueEngine) || meth.isNullOrEmpty(typeBody) ||
+                    meth.isNullOrEmpty(vinNumber) || dateCreated == null || meth.isNullOrEmpty(carColor) ||
+                    meth.isNullOrEmpty(userAdres) || meth.isNullOrEmpty(typeAuto)) {
                 System.out.println("Одно или несколько полей пустые.");
-                return;// останавливаем выполнение функции в случае если одно из полей пустое
-            }System.out.println(name + "/ " + model + "/ " + typeAuto + "/" + numbersCars + "/ " + typeUser + "/ " + valueEngine + "/ " + typeBody
-                    + "/ " + vinNumber + "/ " + dateCreated + "/ " + carColor + "/ " + userAdres); // выводим если все заполнено и ок.
-            //проверка даты , чтоб не была больше текущей.
+                return; // Останавливаем выполнение функции в случае, если одно из полей пустое
+            }
+
+            // Проверяем корректность даты
             if (dateCreated.isAfter(currentDate)) {
-                System.out.println("Выбранная данная является некорректной");
+                System.out.println("Выбранная дата является некорректной.");
+                return;
+            }
+
+            // Создаем объект InfoCarsUsers для дальнейшей работы
+            InfoCarsUsers carUser = new InfoCarsUsers(model, typeAuto, typeUser, numbersCars, dateCreated, name, userAdres,
+                    valueEngine, carColor, typeBody, vinNumber);
+
+            // Переходим к записи данных в базу данных
+            String sqlInsert = "INSERT INTO \"InformationOfUsers\" (\"Model\", \"TypeOfCar\", \"TypeOfBody\", \"TypeOfUser\", \"FederationNumber\", \"VinNumber\", \"DateCreate\", \"NameOfUser\", \"Adress\", \"ValueEngine\", \"Color\") " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) " +
+                    "ON CONFLICT (\"VinNumber\", \"FederationNumber\") DO NOTHING";
+
+            try (Connection conn = connect();
+                 PreparedStatement pstmt = conn.prepareStatement(sqlInsert)) {
+
+                pstmt.setString(1, model);
+                pstmt.setString(2, typeAuto);
+                pstmt.setString(3, typeBody);
+                pstmt.setString(4, typeUser);
+                pstmt.setString(5, numbersCars); // FederationNumber
+                pstmt.setString(6, vinNumber); // VinNumber
+                pstmt.setDate(7, java.sql.Date.valueOf(dateCreated));
+                pstmt.setString(8, name);
+                pstmt.setString(9, userAdres);
+                pstmt.setString(10, valueEngine);
+                pstmt.setString(11, carColor);
+
+                pstmt.executeUpdate();
+                System.out.println("Данные успешно добавлены или уже существуют.");
+            } catch (SQLException e) {
+                System.out.println("Ошибка при добавлении данных в базу данных: " + e.getMessage());
+                e.printStackTrace();
             }
         }
         //#endregion
+        //#region Функция для метода добавления информации в БД
+        public ObservableList<InfoCarsUsers> executeQueryAndGetData(String sql) {
+            ObservableList<InfoCarsUsers> data = FXCollections.observableArrayList();
+            try (Connection conn = DriverManager.getConnection(url, name, pass);
+                 Statement stmt = conn.createStatement();
+                 ResultSet rs = stmt.executeQuery(sql)) {
 
+                while (rs.next()) {
+                    String carBrand = rs.getString("Model");
+                    String carType = rs.getString("TypeOfCar");
+                    String personType = rs.getString("TypeOfUser");
+                    String licensePlate = rs.getString("FederationNumber");
+                    LocalDate year = rs.getDate("DateCreated").toLocalDate();
+                    String ownerName = rs.getString("NameOfUser");
+                    String address = rs.getString("Adress");
+                    String engineVolume = rs.getString("ValueEngine");
+                    String color = rs.getString("Color");
+                    String bodyType = rs.getString("TypeOfBody");
+                    String vin = rs.getString("VinNumber");
+
+                    InfoCarsUsers userInfo = new InfoCarsUsers(carBrand, carType, personType, licensePlate, year, ownerName, address, engineVolume, color, bodyType, vin);
+                    data.add(userInfo);
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                // Обработка ошибок подключения и запроса
+            }
+
+            return data;
+        }
+        //#endregion
+        //#region Создание функции вывода информации в таблицу по заданной выборке
+        @FXML
+        public void LooksAtUsers() {
+            // Записываем в переменные данные, введенные пользователем
+            StrinMethods meth = new StrinMethods();
+            String name = nameOfUser.getText();
+            String model = modelOfCar.getText();
+            String numbersCars = numberOfCar.getText();
+            String typeUser = typeOfUser.getText();
+            String valEng = valueOfEngine.getText();
+            String typeBody = typeOfBody.getText();
+            String vinNumber = numberOfFederation.getText();
+            LocalDate dateCreated = dateOfCreatCar.getValue();
+            String carColor = colorOfCar.getText();
+            String userAdres = adressOfUser.getText();
+            String typeAuto = getTypeAuto();
+
+            // Создаем список с названиями столбцов из БД
+            List<String> allColumnNames = new ArrayList<>();
+            meth.addColumnNames(allColumnNames); // Добавили все названия столбцов
+
+            // Создаем список со всеми введенными значениями
+            List<String> allInputValues = new ArrayList<>();
+            meth.addValueIntoList(name, model, numbersCars, typeUser, valEng, typeBody, vinNumber, carColor, userAdres, typeAuto, allInputValues);
+
+            // Создаем мапу для хранения непустых значений
+            Map<String, String> listNoNullValues = new HashMap<>();
+            meth.addNewValuesNoneNullIntoList(allInputValues, listNoNullValues, allColumnNames); // Прогоняем список и добавляем непустые значения в мапу
+
+            System.out.println(listNoNullValues); // Для отладки: выводим список непустых значений
+
+            // Генерируем SQL-запрос
+            String sql = meth.generateSelectQuery("public.\"InformationOfUsers\"", listNoNullValues, dateCreated);
+            System.out.println(sql); // Для отладки: выводим запрос на экран
+
+            // Выполняем запрос и получаем данные
+            ObservableList<InfoCarsUsers> data = executeQueryAndGetData(sql); // Передаем SQL-запрос в метод
+
+            // Устанавливаем полученные данные в таблицу
+            MyTableFirstPage.setItems(data);
+        }
+        //#endregion
+
+        // Тут они заканчиваются
         //#region Создаем слушатель событий для первой страницы с выпдающим списком
         private String typeAuto;
 
@@ -174,7 +316,7 @@
 
         //#endregion
 
-        //#region Добавление в выпадающие списки варианты выбора
+        //#region Инициализация приложения
         @FXML
         public void initialize() {
             // Инициализация ComboBox
@@ -184,12 +326,14 @@
 
             comBoxThirdPage.getItems().addAll("Наезд на ограждение, столб", "Наезд на пешехода", "Лобовое столкновение", "Наезд на транспорт стоящий спереди", "Боковое столкновение на перекрестке");
 
-            comBoxThourPage.getItems().addAll("Легковой", "Грузовой", "Мотоцикл", "Трактор", "Полуприцеп", "Автобус", "МикроАвтобус");
             comBoxFirstPage.valueProperty().addListener((observable, oldValue, newValue) -> {  // тут лямба функция, я не понимаю как точно она работает поэтому запомнить нужно конструкцию
                 typeAuto = newValue; // Сохраняем выбранное значение в поле
             });
+            initializeTableColumns();
+
         }
         //#endregion
+
         //#region Подключение к БД
         public static Connection connect() {
             Connection connection = null;
@@ -203,16 +347,100 @@
             return connection;
         }
         //#endregion
+
+        //#region функция инициализации столбцов
+        public void initializeTableColumns() {
+            carBrandColumn.setCellValueFactory(new PropertyValueFactory<>("carBrand"));
+            carTypeColumn.setCellValueFactory(new PropertyValueFactory<>("carType"));
+            personTypeColumn.setCellValueFactory(new PropertyValueFactory<>("personType"));
+            licensePlateColumn.setCellValueFactory(new PropertyValueFactory<>("licensePlate"));
+            yearColumn.setCellValueFactory(new PropertyValueFactory<>("year"));
+            ownerNameColumn.setCellValueFactory(new PropertyValueFactory<>("ownerName"));
+            addressColumn.setCellValueFactory(new PropertyValueFactory<>("address"));
+            engineVolumeColumn.setCellValueFactory(new PropertyValueFactory<>("engineVolume"));
+            colorColumn.setCellValueFactory(new PropertyValueFactory<>("color"));
+            bodyTypeColumn.setCellValueFactory(new PropertyValueFactory<>("bodyType"));
+            vinColumn.setCellValueFactory(new PropertyValueFactory<>("vin"));
+        }
+        //#endregion
+
     }
 
     class StrinMethods {
-        private boolean isEmpty;
-        public boolean isNullOrEmpty(String text) {
-            if (text == "" || text == null) {
-                isEmpty = false;
-            }else {
-                isEmpty = true;
-            }
-            return isEmpty;
+        private boolean isEmpty; // переменная для метода
+        //#region Метод проверки строки на пустую или же значение null
+        public boolean isNullOrEmpty(String str) {
+            return str == null || str.trim().isEmpty(); // Проверяет, является ли строка null или пустой
         }
+        //#endregion
+
+        //#region Метод добавления моих переменных для первой странички в массив
+        public void addValueIntoList(String name, String model, String numbersCars, String typeUser, String valueEngine,
+                                     String typeBody, String vinNumber, String carColor, String userAdres, String typeAuto,
+                                     List<String> allInputValues) {
+            allInputValues.add(name);         // NameOfUser
+            allInputValues.add(model);        // Model
+            allInputValues.add(numbersCars);  // FederationNumber
+            allInputValues.add(typeUser);     // TypeOfUser
+            allInputValues.add(valueEngine);  // ValueEngine
+            allInputValues.add(typeBody);     // TypeOfBody
+            allInputValues.add(vinNumber);    // VinNumber
+            allInputValues.add(carColor);     // Color
+            allInputValues.add(userAdres);    // Adress
+            allInputValues.add(typeAuto);
+        }
+        //#endregion
+
+        //#region Метод для того чтобы пробежаться по списку List<String> и записать в новый список List<Strin> уже не пустые
+        public void addNewValuesNoneNullIntoList(List<String> allInputValues, Map<String, String> NewValues, List<String> columnNames) {
+            for(int i = 0; i < allInputValues.size(); i++) {
+                if(!isNullOrEmpty(allInputValues.get(i))) {
+                    NewValues.put(columnNames.get(i), allInputValues.get(i));
+                }
+            }
+        }
+        //#endregion
+
+        //#region Метод добавления названия столбцов в список
+        public void addColumnNames(List<String> ColumnNames) {
+            ColumnNames.add("NameOfUser");
+            ColumnNames.add("Model");
+            ColumnNames.add("FederationNumber");
+            ColumnNames.add("TypeOfUser");
+            ColumnNames.add("ValueEngine");
+            ColumnNames.add("TypeOfBody");
+            ColumnNames.add("VinNumber");
+            ColumnNames.add("Color");
+            ColumnNames.add("Adress");
+            ColumnNames.add("TypeOfCar");
+        }
+        //#endregion
+
+        //#region Метод генерации запроса на основе заполненных полей
+        public String generateSelectQuery(String tableName, Map<String, String> data, LocalDate dateCreated) {
+            // Строим часть для столбцов и значений
+            StringBuilder whereClause = new StringBuilder();
+            boolean first = true;
+
+            // Добавляем параметры из data
+            for (Map.Entry<String, String> entry : data.entrySet()) {
+                if (!first) {
+                    whereClause.append(" AND ");
+                }
+                whereClause.append("\"").append(entry.getKey()).append("\" = '").append(entry.getValue()).append("'");
+                first = false;
+            }
+
+            // Проверяем дату и добавляем её в запрос, если она не пустая
+            if (dateCreated != null) {
+                if (whereClause.length() > 0) {
+                    whereClause.append(" AND "); // Добавляем AND, если уже есть условия
+                }
+                whereClause.append("\"DateCreated\" = '").append(dateCreated.toString()).append("'"); // Преобразуем дату в строку
+            }
+
+            // Возвращаем финальный SQL-запрос
+            return "SELECT * FROM " + tableName + " WHERE " + whereClause.toString() + ";";
+        }
+        //#endregion
     }
